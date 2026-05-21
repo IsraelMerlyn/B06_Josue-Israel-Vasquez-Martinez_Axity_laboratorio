15213c50997b0e7af5f3eae7675ac9d25bf5c039
IsraelMerlyn <merlyn142a@gmail.com>
2026-05-21 09:15:52 -0600

    levantamiento del proyecto y configuracion de liquibase

---
+++ src/main/java/com/b06_josueisraelvasquezmartinez/bloque4_axity/Bloque4AxityApplication.java
@@ -0,0 +1,13 @@
+package com.b06_josueisraelvasquezmartinez.bloque4_axity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bloque4AxityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bloque4AxityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b06_josueisraelvasquezmartinez/bloque4_axity/Bloque4AxityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b06_josueisraelvasquezmartinez.bloque4_axity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bloque4AxityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
